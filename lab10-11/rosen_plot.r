6cd6f10aa88e6dda24ab02c9ec2c6ff9dfe97d0b
matmark <mat.mark@wp.pl>
2022-05-20 01:47:21 +0200

    Contour plot and heatmap

--- lab10-11/rosen_plot.r
+++ lab10-11/rosen_plot.r
@@ -23,7 +23,59 @@ show_3d_plot <- function() {
     ) %>%
         add_trace(0,
             x = 1, y = 1, z = 0, mode = "markers", type = "scatter3d",
-            marker = list(size = 5, color = "red", symbol = 104)
+            marker = list(size = 5, color = "red", symbol = 'x')
+        )
+
+    fig <- fig %>% layout()
+    print(fig)
+}
+
+show_heatmap <- function() {
+    x <- seq(-10, 10, by = 0.1)
+    y <- seq(-10, 10, by = 0.1)
+    z <- array(0, dim = c(length(x), length(y)))
+    for (i in 1:length(x)) {
+        for (j in 1:length(y)) {
+            z[i, j] <- rosen(c(x[i], y[j]))
+        }
+    }
+
+    fig <- plot_ly(
+        type = "heatmap",
+        colorscale='Jet',
+        x = ~x,
+        y = ~y,
+        z = ~z
+    ) %>%
+        add_trace(0,
+            x = 0, y = 0, z = 0, mode = "markers", type = "scatter",
+            marker = list(size = 10, color = "white", symbol='x')
+        )
+
+    fig <- fig %>% layout()
+    print(fig)
+}
+
+show_contour <- function() {
+    x <- seq(-10, 10, by = 0.1)
+    y <- seq(-10, 10, by = 0.1)
+    z <- array(0, dim = c(length(x), length(y)))
+    for (i in 1:length(x)) {
+        for (j in 1:length(y)) {
+            z[i, j] <- rosen(c(x[i], y[j]))
+        }
+    }
+
+    fig <- plot_ly(
+        type = "contour",
+        colorscale='Jet',
+        x = ~x,
+        y = ~y,
+        z = ~z
+    ) %>%
+        add_trace(0,
+            x = 0, y = 0, z = 0, mode = "markers", type = "scatter",
+            marker = list(size = 10, color = "white", symbol='x')
         )
 
     fig <- fig %>% layout()
@@ -31,3 +83,5 @@ show_3d_plot <- function() {
 }
 
 show_3d_plot()
+show_heatmap()
+show_contour()

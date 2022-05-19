5f704e45e76938a66252ed75970f8dddf1e3c62b
matmark <mat.mark@wp.pl>
2022-05-19 21:30:55 +0200

    Some results

--- lab10-11/ackley_tests.r
+++ lab10-11/ackley_tests.r
@@ -8,50 +8,31 @@ source("lab10-11/ackley_plot.r")
 opt_ackley <- getGlobalOpt("Ackleys")
 bounds_ackley <- getDefaultBounds("Ackleys")
 
-show_plot()
+# show_plot()
 
 # settings for genetic
-config_name <- "Iterations"
-config_values <- c(100, 250, 500, 1000, 2000)
+# default settings:
+# population = 50,
+# iterations = 1000,
+# cross_probability = 0.8,
+# mut_probability = 0.1,
+# elite_population = 2
+
+config_name <- "Elite_population"
+config_values <- c(0, 1, 2, 4, 8)
 config <- list()
-config[[1]] <- c(
-    population = 50,
-    iterations = config_values[1],
-    cross_probability = 0.8,
-    mut_probability = 0.1,
-    elite_population = 2
-)
-config[[2]] <- c(
-    population = 50,
-    iterations = config_values[2],
-    cross_probability = 0.8,
-    mut_probability = 0.1,
-    elite_population = 2
-)
-config[[3]] <- c(
-    population = 50,
-    iterations = config_values[3],
-    cross_probability = 0.8,
-    mut_probability = 0.1,
-    elite_population = 2
-)
-config[[4]] <- c(
-    population = 50,
-    iterations = config_values[4],
-    cross_probability = 0.8,
-    mut_probability = 0.1,
-    elite_population = 2
-)
-config[[5]] <- c(
-    population = 50,
-    iterations = config_values[5],
-    cross_probability = 0.8,
-    mut_probability = 0.1,
-    elite_population = 2
-)
+for (i in seq_len(config_values)) {
+    config[[i]] <- c(
+        population = 50,
+        iterations = 1000,
+        cross_probability = 0.8,
+        mut_probability = 0.1,
+        elite_population = config_values[i]
+    )
+}
 
 global_avg_results <- c()
-for (param in 1:5) {
+for (param in seq_len(config_values)) {
     results <- c()
     for (i in 1:10) {
         genetic <- ga(
@@ -73,8 +54,8 @@ for (param in 1:5) {
     # print("All results:")
     # print(results)
     # mean of 10 iterations
-    print("Mean of 10 iterations:")
-    print(sum(results) / 10)
+    # print("Mean of 10 iterations:")
+    # print(sum(results) / 10)
     # optimum value
     # print("Optimum value:")
     # print(opt_ackley)
@@ -84,4 +65,8 @@ df <- data.frame(
     config_name = config_values,
     avg_results = global_avg_results
 )
-write.csv(df, paste("lab10-11/", config_name, "_ackley.csv"), row.names = TRUE)
+write.csv(
+    df,
+    paste("lab10-11/ackley_results/", config_name, "_ackley.csv"),
+    row.names = TRUE
+)

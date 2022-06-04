2e032c01d49fa0ccab8952c051c1db4ba4c7366c
matmark <mat.mark@wp.pl>
2022-06-04 03:36:29 +0200

    Fix crossover

--- lab12-14/a/crossover.r
+++ lab12-14/a/crossover.r
@@ -1,22 +1,27 @@
 source("lab12-14/a/ackley.r")
-# zamienia ze soba x jednego z rodzicow z y drugiego rodzica
-# (wymiana x z x lub y z y miedzy rodzicami nie ma wplywu na ta funkcje)
+# zamienia ze soba x lub y jednego z rodzicow z odpowiednia wspolrzedna drugiego rodzica
 # np.
 # rodzice:
-#           [,1]      [,2]
-# [1,]  4.410435 -8.091990
-# [2,] -3.571726  1.054254
+#            [,1]       [,2]
+# [1,]  1.0742502  1.0742502
+# [2,] -0.9913074 -0.1522789
 #
 # dzieci:
-#           [,1]      [,2]
-# [1,]  1.054254 -8.091990
-# [2,] -3.571726  4.410435
+#            [,1]       [,2]
+# [1,] -0.9913074  1.0742502
+# [2,]  1.0742502 -0.1522789
 crossover_function <- function(object, parents, ...) {
     f <- object@fitness[parents]
     parents <- object@population[parents, , drop = FALSE]
     offsprings <- parents
-    offsprings[1, 1] <- parents[2, 2]
-    offsprings[2, 2] <- parents[1, 1]
+    x <- sample(c(TRUE, FALSE), 1)
+    if (x) {
+        offsprings[1, 1] <- parents[1, 2]
+        offsprings[1, 2] <- parents[1, 1]
+    } else {
+        offsprings[2, 1] <- parents[1, 1]
+        offsprings[1, 1] <- parents[2, 1]
+    }
     f[1] <- -ackley(offsprings[1, ])
     f[2] <- -ackley(offsprings[2, ])
     out <- list(
